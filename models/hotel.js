0462b91fa4455e85b04fd0a9c3f3555800982e68
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2025-11-05 10:22:20 +0700

    inisiasi semua package dan koneksi database pada env

---
+++ models/hotel.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) =>{
+    const Hotel = sequelize.define("Hotel", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Tipe_Kamar: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Kapasitas_Tamu: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        Lantai: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Fasilitas: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Tanggal_Pesan: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+
+    }, {
+        tableName: "hotel",
+        freezeTableName: true,
+        timestamps: true
+    });
+    return Hotel;
+}
